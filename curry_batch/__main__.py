f2bf7c91eabb297ea9e23a1bfee468862fb7e475
Ben Skubi <bskubi@gmail.com>
2024-02-06 12:15:29 -0800

    Added shell=True/False option

--- curry_batch/__main__.py
+++ curry_batch/__main__.py
@@ -12,23 +12,27 @@ def worker_init():
 
 def execute_command(args):
     env = os.environ.copy()
-    command, arg_list, dryrun = args
+    command, arg_list, shell, dryrun = args
     wc_dict = enumerate_wildcards(arg_list)
     try:
-        formatted_command = safe_format(command, wc_dict)
+        if shell:
+            formatted_command = safe_format(command, wc_dict)
+        else:
+            command_list = yaml.safe_load(command)
+            formatted_command = [safe_format(arg, wc_dict) for arg in command_list]
     except Exception as e:
         print(f"Error in execute_command for arg_list {arg_list} and command template {command}: {e}")
     if dryrun:
         click.echo(formatted_command)
         result = ''
     else:
-        result = subprocess.run(formatted_command, shell=True, capture_output=True, text=True, env=env)
+        result = subprocess.run(formatted_command, shell=shell, capture_output=True, text=True, env=env)
         result = result.stdout.strip()
     return result
 
-def execute_commands_multiprocessing(commands, arguments, max_processes=None, dryrun=False):
+def execute_commands_multiprocessing(commands, arguments, shell=True, max_processes=None, dryrun=False):
     output = []
-    args_for_pool = [(command, args, dryrun) for args in arguments for command in commands]
+    args_for_pool = [(command, args, shell, dryrun) for args in arguments for command in commands]
 
     with Pool(processes=max_processes, initializer=worker_init) as pool:
         try:
@@ -60,12 +64,15 @@ def enumerate_wildcards(arguments):
 
 @click.command()
 @click.argument('commands', nargs=-1, required=True)
+@click.option("--shell/--no-shell", is_flag=True, default=True, help="Run with shell=True (--shell) or with shell=False and interpreting commands as lists of argumnets (--no-shell)")
 @click.option('--max-processes', default=None, help='Limit number of processes to use for parallel execution')
 @click.option('--dryrun', is_flag = True, help='Echo commands to stdout rather than running them')
-def curry_batch(commands, max_processes, dryrun):
+def curry_batch(commands, shell, max_processes, dryrun):
     input = sys.stdin.read()
     arguments = yaml.safe_load(input)
-    output = execute_commands_multiprocessing(commands, arguments, max_processes, dryrun)
+    if dryrun:
+        print("Curry-batch arguments:", arguments)
+    output = execute_commands_multiprocessing(commands, arguments, shell, max_processes, dryrun)
     if not dryrun:
         click.echo(yaml.dump(output))
 
